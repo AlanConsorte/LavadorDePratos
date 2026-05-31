b3697999c39d422a1f0d1d23eb591901d7ffecac
AlanConsorte <alanfconsorte@gmail.com>
2026-05-31 16:58:29 -0300

    finalização

--- src/PratosSujosFactory.java
+++ src/PratosSujosFactory.java
@@ -1,32 +1,31 @@
 import java.util.Random;
 import java.util.logging.Logger;
 
-
 public class PratosSujosFactory {
     private int i = 1;
 
     private static final Logger logger = Logger.getLogger(PratosSujosFactory.class.getName());
-    
+
     Random r = new Random();
-    
-    public Prato entregaPrato(){
+
+    public Prato entregaPrato() {
         Prato p = new Prato();
-        
+
         p.setNumSerie(i);
         i++;
-        
+
         int x = r.nextInt(100);
-        
-        if(x < 10){
+
+        if (x < 10) {
             p.setSujeira(nivelSujeira.ENGORDURADO);
-        } else if (x >= 10 && x <= 70){
+        } else if (x >= 10 && x <= 70) {
             p.setSujeira(nivelSujeira.MEDIO);
-        } else if(x > 70) {
+        } else if (x > 70) {
             p.setSujeira(nivelSujeira.BAIXO);
         }
-        
+
         logger.info("FÁBRICA: Novo prato criado! ID: " + p.getNumSerie() + " | Sujeira: " + p.getSujeira());
-        
+
         return p;
     }
 }
